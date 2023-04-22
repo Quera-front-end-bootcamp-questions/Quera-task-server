8db60b874ff50b77c0778bfcf0d69044347f955d
desertfoox <salar.nili097@gmail.com>
2023-04-22 16:13:53 +0330

    add them in app

--- src/App/App.ts
+++ src/App/App.ts
@@ -1,29 +1,30 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
-
-import dbConnect from "../Db/DbConnection"; // import the dbConnect function
-
-import userRouter from '../Routes/Auth/Auth.Route';
-
+import dbConnect from "../Db/DbConnection";
+import projectRouter from "../Routes/Project/Project.Route";
+import tagRouter from "../Routes/Tag/Tag.Route";
+import userRouter from "../Routes/User/User.Route";
+import authRouter from "../Routes/Auth/Auth.Route";
 require("dotenv").config();
 
-const App = express();
+const app = express();
 
-App.use(morgan("dev"));
-App.use(helmet());
-App.use(cors());
-App.use(express.json());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
-// call the dbConnect function
 dbConnect();
 
-App.use('/api/user', userRouter);
+app.use("/api/projects", projectRouter);
+app.use("/api/tags", tagRouter);
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
 
-
-App.get("/", async (req: Request, res: Response) => {
-  res.send("Download postman file");
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
-export default App;
+export default app;
