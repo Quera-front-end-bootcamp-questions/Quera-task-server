0e47bb773afda7372fbc7cad8efa6e1088f4acb0
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2023-05-25 14:02:41 +0330

    sendResponse function fixed

--- src/Utils/SendResponse.ts
+++ src/Utils/SendResponse.ts
@@ -10,8 +10,8 @@ interface ResponseData<T> {
 export const sendResponse = <T>(res: Response, statusCode: number, data?: any, message?: string) => {
   const responseData: ResponseData<T> = {
     message: message || '',
-    code: statusCode,
-    error: false,
+    code: statusCode,  
+    error: (statusCode <400 && statusCode > 199) ? false : true,
     data: data || null
   };
   return res.status(statusCode).send(responseData);
